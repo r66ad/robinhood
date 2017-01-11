f92328c23379ac16a255dd8b1d9d17aa1aff8183
Gabriele Iannetti <g.iannetti@gsi.de>
2017-01-11 08:41:05 +0100

    Removed Filename Filter

--- web_gui/gui_v3/index.php
+++ web_gui/gui_v3/index.php
@@ -104,9 +104,6 @@ foreach ($fields as $field) {
     <fieldset class="form-group">
     <input type="text" class="form-control" id="formGID" name="gid" placeholder="GID">
     </fieldset>
-    <fieldset class="form-group">
-    <input type="text" class="form-control" id="formFilename" name="filename" placeholder="Filename">
-    </fieldset>
     <button type="button" class="btn btn-primary" id="myStateButton" data-loading-text="Loading..." autocomplete="off">Filter</button>
     <button type="button" class="btn btn-primary" onclick="CleanForm();GetGraph(lastGet)">Clean</button>
     </form>
